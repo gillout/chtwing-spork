97b5131b86e5648924bff897bb994d04878e73e8
gillout <54893913+gillout@users.noreply.github.com>
2023-03-12 14:57:26 +0100

    Ajout de Bootstrap, et de polices de caractères et d'icons de Google

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', loadChildren: () => import('./home/home.module').then(module => module.HomeModule)},
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: '**', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
